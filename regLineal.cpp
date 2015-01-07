70b05850e82e6f264d871b20ec9be6d3dbddc76f
Cesar_Snake <snakemgs14@gmail.com>
2015-01-07 12:10:34 +0100

    subido

--- regLineal.cpp
+++ regLineal.cpp
@@ -25,7 +25,8 @@ void procesarLinea(string linea, int numMuestra)
         if(linea[i] != delimiter && linea[i] != '\n')
         {
             aux += linea[i];
-        }else
+        }
+        else
         {
             switch(pos)
             {
@@ -67,7 +68,8 @@ int main(int argc, char* argv[])
 
                 numMuestra ++;
             } 
-        }else
+        }
+        else
         {
             cout << "ERROR: " << nombreFicheroEntrada << " no encontrado" << endl;
         }
@@ -79,7 +81,8 @@ int main(int argc, char* argv[])
             cout << sube[i] << "," << open[i] << "," << close[i] << endl;
         }
 
-    }else
+    }
+    else
     {
         cout << "ERROR: Número de parámetros incorrecto." << endl;
         cout << "Ejemplo de uso:" << endl;
