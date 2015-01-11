44c4451429092774ec7d9ee798159ca09d893e9a
jgm88 <jgm79@outlook.com>
2015-01-11 21:08:46 +0100

    Cambiadas cosicas de LinR

---
+++ include/multiLinearRegression.h
@@ -0,0 +1,19 @@
+#ifndef LINEARREGRESSION_H
+#define LINEARREGRESSION_H
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
+
+#include <armadillo>
+
+class MultiLinearRegression
+{
+public:
+
+
+private:
+
+};
+
+
+#endif
