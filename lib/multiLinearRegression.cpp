016bcc1532b461a8d5b3f4a57c5899bdbcea2bad
jgm88 <jgm79@outlook.com>
2015-01-12 20:56:56 +0100

    version sin NN

--- lib/multiLinearRegression.cpp
+++ lib/multiLinearRegression.cpp
@@ -14,8 +14,8 @@ void train(int iterations, int numData, std::vector<int> vClass, std::vector<dou
 	//Obtenemos la traspuesta
 	mat Xt = X.t();
 
-	// //traspuesta por original
-	mat XtX = Xt * X;
+	// // //traspuesta por original
+	// mat XtX = Xt * X;
 
 	// //Calculamos la inversa de X*Xt
 	// mat invXtX = XtX.i(); 
